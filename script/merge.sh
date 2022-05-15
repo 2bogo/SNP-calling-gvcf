d9a8456a8aed0354b654d3b52d24aa6c77fbe2a8
yeah-zin <justezy0210@gmail.com>
2022-05-15 11:05:45 +0900

    bcftools N_ALT, filtering

--- script/merge.sh
+++ script/merge.sh
@@ -2,9 +2,10 @@ REF=$1
 OUTPUT=$2
 
 bcftools merge -g ${REF} -m both -Oz -o ${OUTPUT}.gz ./gvcf/*.gvcf.gz &&
-
 bcftools view -i 'F_MISSING < 0.05' -Oz -o ${OUTPUT}.missing.gz ${OUTPUT}.gz &&
-
-bcftools filter -sLowQual -g3 -G10 -e'%QUAL<10 || (RPB<0.1 && %QUAL<15) || (AC<2 && %QUAL<15)' ${OUTPUT}.missing.gz > ${OUTPUT}.filter.gz
+bcftools view -e 'N_ALT!=0' -Oz -o ${OUTPUT}.alt.gz ${OUTPUT}.missing.gz &&
+rm ${OUTPUT}.missing.gz &&
+bcftools filter -sLowQual -g3 -G10 -e'%QUAL<10 || (RPB<0.1 && %QUAL<15) || (AC<2 && %QUAL<15)' -Oz -o ${OUTPUT}.filter.gz ${OUTPUT}.alt.gz &&
+rm ${OUTPUT}.alt.gz
 
 echo "finish"
